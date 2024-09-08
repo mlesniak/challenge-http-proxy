0c7a1f46e7e4290cee26141db2d4c377ac0f88eb
Michael Lesniak <mail@mlesniak.com>
2024-09-08 14:59:27 +0200

    Add demo how to connect to SSL-secured connection from Java

--- src/main/java/com/mlesniak/Main.java
+++ src/main/java/com/mlesniak/Main.java
@@ -3,8 +3,12 @@ package com.mlesniak;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.security.KeyStore;
 
 public class Main {
@@ -26,8 +30,31 @@ public class Main {
 
         SSLServerSocketFactory sslFac = sslctx.getServerSocketFactory();
 
-        ServerSocket sock = sslFac.createServerSocket(8989);
-        var client = sock.accept();
-        ProxyServer.handle(client);
+        new Thread(() -> {
+            ServerSocket sock = null;
+            try {
+                sock = sslFac.createServerSocket(8989);
+                var client = sock.accept();
+                // ProxyServer.handle(client);
+                IOUtils.dump(client.getInputStream());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+
+        // Create a certificate:
+        // keytool -exportcert -alias proxy -file servercert.crt -keystore keystore.jks
+        //
+        // Create a truststore or import it, respectively.
+        // keytool -importcert -alias proxy -file servercert.crt -keystore truststore.jks
+        //
+        // Create an SSL socket and connect to the server
+        System.setProperty("javax.net.ssl.trustStore", "truststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "password");
+        Thread.sleep(1000);
+        SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        try (Socket socket = sslSocketFactory.createSocket("localhost", 8989)) {
+           socket.getOutputStream().write("Hello".getBytes());
+        }
     }
 }
